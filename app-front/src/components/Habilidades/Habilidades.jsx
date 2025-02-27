cec824c5cbbee2203b45e51e270513f4ba3e03f3
viniciusdevegili <viniciusdevegili@hotmail.com>
2025-02-27 18:51:16 -0300

    tela de habilidades e formação finalizada

--- app-front/src/components/Habilidades/Habilidades.jsx
+++ app-front/src/components/Habilidades/Habilidades.jsx
@@ -1,15 +1,54 @@
+import React from "react";
+import "./Habilidades.css";
+
 function Habilidades() {
-    return (
-        <div>
-        <h1>Habilidades</h1>
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JavaScript</p>
-        <p>React</p>
-        <p>Node</p>
-        <p>SQL</p>
+  return (
+    <section id="habilidades" className="habilidades-container">
+      <div className="habilidades">
+        <div className="habilidades-text">
+          <h2>Habilidades</h2>
         </div>
-    );
+        <div className="card-container">
+          <div className="card1">
+            <img src="../src/assets/powerbi.png" alt="Formacao" />
+            <a href="#Conhecimentos" className="botao-conhecimentos">
+              Conhecimentos
+            </a>
+            <a href="#projetos" className="botao-projetos">
+              Ver Projetos
+            </a>
+          </div>
+          <div className="card2">
+            <img src="../src/assets/excel.png" alt="Formacao" />
+            <a href="#Conhecimentos" className="botao-conhecimentos">
+              Conhecimentos
+            </a>
+            <a href="#projetos" className="botao-projetos">
+              Ver Projetos
+            </a>
+          </div>
+          <div className="card3">
+            <img src="../src/assets/python.png" alt="Formacao" />
+            <a href="#Conhecimentos" className="botao-conhecimentos">
+              Conhecimentos
+            </a>
+            <a href="#projetos" className="botao-projetos">
+              Ver Projetos
+            </a>
+          </div>
+          <div className="card4">
+            <img src="../src/assets/sql.png" alt="Formacao" />
+            <a href="#Conhecimentos" className="botao-conhecimentos">
+              Conhecimentos
+            </a>
+            <a href="#projetos" className="botao-projetos">
+              Ver Projetos
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Habilidades;
--- app-front/src/components/Projetos/Projetos.jsx
+++ app-front/src/components/Projetos/Projetos.jsx
@@ -1,10 +1,42 @@
+import React from "react";
+import "./Projetos.css";
+
 function Projetos() {
-    return (
-        <div>
-        <h2>Projetos</h2>
-        <p>Em construção...</p>
+  return (
+    <section id="projetos" className="projetos-container">
+      <div className="projetos">
+        <div className="projetos-text">
+          <h2>Projetos em Destaque</h2>
         </div>
-    );
+        <div className="projetos-destaque">
+          <div className="projeto1">
+            <h3>Dashboard Comercial</h3>
+            <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="botao">
+              Ver Mais
+            </a>
+          </div>
+          <div className="projeto2">
+          <h3>Dashboard Comercial</h3>
+            <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="botao">
+              Ver Mais
+            </a>
+          </div>
+          <div className="projeto3">
+          <h3>Dashboard Comercial</h3>
+            <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="botao">
+              Ver Mais
+            </a>
+          </div>
+          <div className="projeto4">
+          <h3>Dashboard Comercial</h3>
+            <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="botao">
+              Ver Mais
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projetos;
