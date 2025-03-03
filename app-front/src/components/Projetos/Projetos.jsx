3e63ef84b4edf3af61f59565d7750f63ecbb1805
viniciusdevegili <viniciusdevegili@hotmail.com>
2025-03-03 18:59:10 -0300

    novos projetos

--- app-front/src/components/Projetos/Projetos.jsx
+++ app-front/src/components/Projetos/Projetos.jsx
@@ -11,15 +11,15 @@ function Projetos() {
         <div className="projetos-destaque">
           <div className="projeto1">
             <img src="../src/assets/dash-bi.png" alt="Dashboard Comercial" />
-            <h3>Dashboard Comercial</h3>
+            <h3>Dashboard Comercial - PowerBI</h3>
             <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="btn-vermais">
               Ver Mais
             </a>
           </div>
           <div className="projeto2">
-            <img src="../src/assets/dash-bi.png" alt="Dashboard Comercial" />
-            <h3>Dashboard Comercial</h3>
-            <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="btn-vermais">
+            <img src="../src/assets/bank-sql.png" alt="Dashboard Comercial" />
+            <h3>Análise de Acionistas do Banco - SQL</h3>
+            <a href="https://github.com/viniciusdevegili/bank-project-sql" className="btn-vermais">
               Ver Mais
             </a>
           </div>
