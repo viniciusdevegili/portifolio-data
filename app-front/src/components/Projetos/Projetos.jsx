5267d2913eb9eda3077ed652f50a93aed201ec98
viniciusdevegili <viniciusdevegili@hotmail.com>
2025-03-11 17:31:58 -0300

    imagem

--- app-front/src/components/Projetos/Projetos.jsx
+++ app-front/src/components/Projetos/Projetos.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Projetos.css";
+import dashBi from "../assets/dash-bi.png";
+import bankSql from "../assets/bank-sql.png";
 
 function Projetos() {
   return (
@@ -10,14 +12,14 @@ function Projetos() {
         </div>
         <div className="projetos-destaque">
           <div className="projeto1">
-            <img src="../src/assets/dash-bi.png" alt="Dashboard Comercial" />
+          <img src={dashBi} alt="Dashboard Comercial" />
             <h3>Dashboard Comercial - PowerBI</h3>
             <a href="https://app.powerbi.com/view?r=eyJrIjoiMmY4NTFhNDMtNjhjOS00M2QxLWFhM2ItNDkxYzI0ZWUxYzI1IiwidCI6IjA5MTdmZTEwLTU2ZGItNDRiZi1iMWM1LTMxMDYxYWIyMWNmOSJ9" className="btn-vermais">
               Ver Mais
             </a>
           </div>
           <div className="projeto2">
-            <img src="../src/assets/bank-sql.png" alt="Dashboard Comercial" />
+          <img src={bankSql} alt="Análise de Acionistas do Banco" />
             <h3>Análise de Acionistas do Banco - SQL</h3>
             <a href="https://github.com/viniciusdevegili/bank-project-sql" className="btn-vermais">
               Ver Mais
